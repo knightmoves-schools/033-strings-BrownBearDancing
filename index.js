ca6893c2c0d40a25b3230f43597a4a66b9abe678
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-16 14:28:48 -0500

    changed back to let

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var firstName = 'Mark';
-var jobTitle = 'Manager';
-var city = 'Des Moines';
+let firstName = 'Mark';
+let jobTitle = 'Manager';
+let city = 'Des Moines';
 
 let newJobPost = 'Hello ${firstName},
   We have received your application for the position of ${jobTitle} in ${city}.
 We will be in touch soon. Please reach out if you have not heard back from someone
 within the week.
   
-Thank you,
+Sincerely,
 HR person name';
 
 document.getElementByID("result").innerHTML = newJobPost;
+
 
 
 
